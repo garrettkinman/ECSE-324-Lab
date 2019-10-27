f6e1e165de4edf8181788eee9ea4428a22a7ba88
garrettkinman <garrettkinman@gmail.com>
2019-10-27 18:22:57 -0400

    Tiny bug fix

--- G37_Lab2/2.1/getting_started.c
+++ G37_Lab2/2.1/getting_started.c
@@ -1,7 +1,6 @@
 int main() {
 	int a[5] = {1, 20, 3, 4, 5};
 	int min_val = a[0];
-	// TODO
 	int i;
 	for(i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
 		if(a[i] < min_val) {
--- G37_Lab2/2.2/getting_started.c
+++ G37_Lab2/2.2/getting_started.c
@@ -3,7 +3,6 @@ extern int MIN_2(int x, int y);
 int main() {
 	int a[5] = {1, 20, 3, 4, 5};
 	int min_val = a[0];
-	return c;
 	int i;
 	for(i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
 		if(MIN_2(min_val, a[i]) < min_val) {
