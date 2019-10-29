23ed64b8a4b6d046d992d5533fdbfffd1c3b1720
garrettk <garrett@humanitas.io>
2019-10-29 12:48:09 -0400

    Filled in slider_switches

--- G37_Lab3/drivers/asm/slider_switches.s
+++ G37_Lab3/drivers/asm/slider_switches.s
@@ -1,4 +1,8 @@
 								.text
+								.equ SW_BASE, 0xFF200040
 								.global read_slider_switches_ASM
 
-read_slider_switches_ASM:		
+read_slider_switches_ASM:		LDR R1, =SW_BASE					// R1 points to where switch info is kept
+								LDR R0, [R1]						// R0 holds switch info
+								BX LR								// return
+								.end
