81742cd569c7e3735356a64ac22d629b77da687d
garrettkinman <garrettkinman@gmail.com>
2019-10-29 23:08:21 -0400

    Tested and works for first part!

--- G37_Lab3/drivers/asm/LEDs.s
+++ G37_Lab3/drivers/asm/LEDs.s
@@ -7,7 +7,7 @@ read_LEDs_ASM:		LDR R1, =LEDR_BASE				// R1 points to where LED info is kept
 					LDR R0, [R1]					// R0 holds LED info
 					BX LR							// return
 					
-write_LEDs_ASM:		LDR R1, =L_BASE					// R1 points to where LED info is kept
+write_LEDs_ASM:		LDR R1, =LEDR_BASE					// R1 points to where LED info is kept
 					STR R0, [R1]					// store value at R0 to where LED info is kept
 					BX LR							// return
 					.end
