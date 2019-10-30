564fa6204ebca72be5c8b6ad4739ec1d6a7ca471
garrettk <garrett@humanitas.io>
2019-10-30 11:52:09 -0400

    Filled in pushbuttons.s and .h
    Need to rewrite a bit

--- G37_Lab3/drivers/inc/pushbuttons.h
+++ G37_Lab3/drivers/inc/pushbuttons.h
@@ -1,60 +1,24 @@
-						.text
-						.equ PUSH_BUTTON_DATA, 0xFF200050
-						.equ PUSH_BUTTON_MASK, 0xFF200058
-						.equ PUSH_BUTTON_EDGE, 0xFF20005C
-						.global read_PB_data_ASM
-						.global PB_data_is_pressed_ASM
-						.global read_PB_edgecap_ASM
-						.global PB_edgecap_is_pressed_ASM
-						.global PB_clear_edgecap_ASM
-						.global enable_PB_INT_ASM
-						.global disable_PB_INT_ASM
+#ifndef __PUSHBUTTONS
+#define __PUSHBUTTONS
 
-read_PB_data_ASM:									//only access the pushbutton data register
-						LDR R1, =PUSH_BUTTON_DATA	
-						LDR R0, [R1]				//load the contents of the pushbutton register into R1
-						BX LR						//USE R0 to pass arguments back		
+	typedef enum {
+		PB0 = 0x00000001,
+		PB1 = 0x00000002,
+		PB2 = 0x00000004,
+		PB3 = 0x00000008
+	} PB_t;
 
-PB_data_is_pressed_ASM:								//R0 contains which button to check, hot-one encoding
-						LDR R1, =PUSH_BUTTON_DATA	
-						LDR R2, [R1]				//load contents of register into R2
-						AND R2, R2, R0
-						CMP R2, R0
-						MOVEQ R0, #1				//True if equal
-						MOVNE R0, #0				//false, the button isnt pressed
-						BX LR
+	/*These subroutines only access the pushbutton data register*/
+	extern int read_PB_data_ASM();
+	extern int PB_data_is_pressed_ASM(PB_t PB);
 
-read_PB_edgecap_ASM:								//no input, only access edgecapture register
-						LDR R1, =PUSH_BUTTON_EDGE	
-						LDR R0, [R1]				//load the contents of the pushbutton register into R1
-						AND R0, R0, #0xF			//Get only edge cap bits
-						BX LR						//USE R0 to pass arguments back		
-		
-PB_edgecap_is_pressed_ASM:							//R0 contains which button to check, hot-one encoding
-						LDR R1, =PUSH_BUTTON_EDGE	
-						LDR R2, [R1]				//load contents of register into R2
-						AND R2, R2, R0
-						CMP R2, R0
-						MOVEQ R0, #1				//True if equal
-						MOVNE R0, #0				//false, the button isnt pressed
-						BX LR
+	/*These subroutines only access the pushbutton edgecapture register*/
+	extern int read_PB_edgecap_ASM();
+	extern int PB_edgecap_is_pressed_ASM(PB_t PB);
+	extern void PB_clear_edgecp_ASM(PB_t PB);
 
-PB_clear_edgecap_ASM:								//R0 contains which pushbutton
-						LDR R1, =PUSH_BUTTON_EDGE
-						MOV R2, R0					//storing any value in edgecap will reset, p.21 (anything but #0) would work
-						STR R2, [R1]
-						BX LR
+	/*These subroutines only access the pushbuton interrupt mask register*/
+	extern void enable_PB_INT_ASM(PB_t PB);
+	extern void disable_PB_INT_ASM(PB_t PB);
 
-enable_PB_INT_ASM:									//R0 contains which button to enable, hot-one encoding
-						LDR R1, =PUSH_BUTTON_MASK
-						AND R2, R0, #0xF			//since pushbuttons has only 4 digits that are used, use 0xF
-						STR R2, [R1]				//store it back into location
-						BX LR
-
-disable_PB_INT_ASM:									//R0 is hot-one encoding of which button to disable
-						LDR R1, =PUSH_BUTTON_MASK	//load mask location
-						LDR R2, [R1]				//load mask bits
-						BIC R2, R2, R0				//AND on the complement of R0
-						STR R2, [R1]				//store it back into the mask
-						BX LR
-						.end
+#endif
