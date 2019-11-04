8f4bc9cc0777b2b92491c3ce1a703d5cab0448dd
garrettk <garrett@humanitas.io>
2019-11-04 11:57:33 -0500

    Added imports

--- G37_Lab3/main.c
+++ G37_Lab3/main.c
@@ -2,6 +2,12 @@
 
 #include "./drivers/inc/LEDs.h"
 #include "./drivers/inc/slider_switches.h"
+#include "./drivers/inc/HEX_displays.h"
+#include "./drivers/inc/pushbuttons.h"
+#include "./drivers/inc/HPS_TIM.h"
+#include "./drivers/inc/ISRs.h"
+#include "./drivers/inc/address_map_arm.h"
+#include "./drivers/inc/int_setup.h"
 
 int main() {
 	
