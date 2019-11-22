446c862fa229cb11db01e1d09b8ee91da5fc01ab
garrettk <garrett@humanitas.io>
2019-11-22 14:58:46 -0500

    Added comments outlining what needs doing

--- G37_Lab4/drivers/asm/VGA.s
+++ G37_Lab4/drivers/asm/VGA.s
@@ -7,12 +7,20 @@
 							// TODO: relevant memory locations for VGA
 
 
-VGA_clear_charbuff_ASM:		// TODO
+VGA_clear_charbuff_ASM:		// should set all the valid memory locations in the character buffer to 0
+							// TODO
 
-VGA_clear_pixelbuff_ASM:	// TODO
+VGA_clear_pixelbuff_ASM:	// should set all the valid memory locations in the pixel buffer to 0
+							// TODO
 
-VGA_write_char_ASM:			// TODO
+VGA_write_char_ASM:			// should store the value of the third input at the memory location calculated from the first two inputs
+							// should check if first two inputs are valid
+							// i.e., that 0 <= x <= 79 and 0 <= y <= 59
+							// TODO
 
-VGA_write_byte_ASM:			// TODO
+VGA_write_byte_ASM:			// should display two characters showing the hex representation of the third input
+							// should check for valid inputs, taking into account that it needs to display two characters
+							// TODO
 
-VGA_draw_point_ASM:			// TODO
+VGA_draw_point_ASM:			// should work very similarly to VGA_write_char_ASM
+							// TODO
